4115741221bb9192cf4afc7f77132189de11800d
Elie Labeca <elie@pandakoo.com>
2017-01-11 12:34:00 +0000

    [EL] - wip - more unit tests - scoverage and scalastyle

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,5 @@
-resolvers += Resolver.url("hmrc-sbt-plugin-releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+resolvers += Resolver.url("hmrc-sbt-plugin-releases",
+  url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
 
 resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
 
@@ -7,5 +8,9 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.4.0")
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.9.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
+
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.8")
---
+++ test/controllers/userJourney/TaxableTurnoverControllerSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.userJourney
+
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+class TaxableTurnoverControllerSpec extends UnitSpec with WithFakeApplication {
+
+  val fakeRequest = FakeRequest("GET", "/taxable/turnover")
+
+  "GET /taxable/turnover" should {
+    "return 200" in {
+      val result = new TaxableTurnoverController().show(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+
+    "return HTML" in {
+      val result = new TaxableTurnoverController().show(fakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+  }
+}
