09e295de5ab1c60056a974db270763c77749cb74
Elie Labeca <elie@pandakoo.com>
2017-01-16 16:33:56 +0000

    [SCRS-4552|RY|EL] - initial commit towards start date controller

---
+++ test/controllers/userJourney/StartDateControllerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.userJourney
+
+import helpers.VatRegSpec
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class StartDateControllerSpec extends VatRegSpec {
+
+  object TestController extends StartDateController {
+    override val authConnector = mockAuthConnector
+  }
+
+  val fakeRequest = FakeRequest(routes.StartDateController.show())
+
+  s"GET ${routes.StartDateController.show()}" should {
+
+    "return HTML" in {
+      callAuthorised(TestController.show, mockAuthConnector) {
+        result =>
+          status(result) mustBe OK
+          contentType(result) mustBe Some("text/html")
+          charset(result) mustBe Some("utf-8")
+          contentAsString(result) must include("VAT taxable turnover")
+      }
+    }
+  }
+
+}
