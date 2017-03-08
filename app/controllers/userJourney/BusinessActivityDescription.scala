8979634968741d8598e86ccdc40c1537978b0bd8
Reddy Yattpu <reddy.yattpu@digital.hmrc.gov.uk>
2017-03-08 16:21:09 +0000

    Create Business Activity Page

--- app/controllers/userJourney/BusinessActivityDescription.scala
+++ app/controllers/userJourney/BusinessActivityDescription.scala
@@ -1,24 +1,3 @@
-package controllers.userJourney
-
-/*
- * Copyright 2017 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-class BusinessActivityDescription {
-
-}
-
 /*
  * Copyright 2017 HM Revenue & Customs
  *
@@ -37,23 +16,25 @@ class BusinessActivityDescription {
 
 package models.view
 
+import models.{ApiModelTransformer, ViewModelTransformer}
+import models.api.{SicAndCompliance, VatScheme}
 import play.api.libs.json.Json
-import
 
-case class BusinessActivityDescription(vatTurnoverEstimate: Option[Long] = None)
+
+case class BusinessActivityDescription(description: String)
 
 object BusinessActivityDescription {
 
   implicit val format = Json.format[BusinessActivityDescription]
 
   implicit val modelTransformer = ApiModelTransformer { (vs: VatScheme) =>
-    vs.si.map(_.turnoverEstimate).collect {
-      case turnoverEstimate => BusinessActivityDescription(Some(turnoverEstimate))
-    }.getOrElse(BusinessActivityDescription())
+    vs.sicAndCompliance.map(_.description).collect {
+      case description => BusinessActivityDescription((description))
+    }.getOrElse(BusinessActivityDescription(""))
   }
 
-  implicit val viewModelTransformer = ViewModelTransformer { (c: BusinessActivityDescription, g: SICAndComplianceType) =>
-    g.copy(turnoverEstimate = c.vatTurnoverEstimate.getOrElse(0L))
+  implicit val viewModelTransformer = ViewModelTransformer { (c: BusinessActivityDescription, g: SicAndCompliance) =>
+    g.copy(description = c.description)
   }
 
 }
