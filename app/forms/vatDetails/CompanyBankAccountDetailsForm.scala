5f6585f0419b68974cd2442ba2c9a16e42b9aa2f
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-03-08 11:56:54 +0000

    validation errors reported on bank account details page

--- app/forms/vatDetails/CompanyBankAccountDetailsForm.scala
+++ app/forms/vatDetails/CompanyBankAccountDetailsForm.scala
@@ -36,7 +36,7 @@ object SortCode {
   }
 
   implicit val show: Show[SortCode] = Show.show(sc => {
-    val str = Seq(sc.part1, sc.part2, sc.part3).mkString("-")
+    val str = Seq(sc.part1.trim, sc.part2.trim, sc.part3.trim).mkString("-")
     //to avoid producing a "--" for sort codes where all double-digits are blank (not entered)
     if (str == "--") "" else str
   })
