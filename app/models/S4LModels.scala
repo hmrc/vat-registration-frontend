9999a112884c287b3aa0eef79979e83e61419070
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-03-31 11:13:47 +0100

    moved files from vatChoice -> vatTradingDetails

--- app/models/S4LModels.scala
+++ app/models/S4LModels.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package models.s4l
+package models
 
 import models.view.sicAndCompliance.{BusinessActivityDescription, CulturalComplianceQ1}
 import models.view.vatFinancials._
