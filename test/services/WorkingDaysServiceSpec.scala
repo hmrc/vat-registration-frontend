808534fcabd809b5be836b1274b87655f07c20cc
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-04-03 20:14:55 +0100

    more tests around adding working days

--- test/services/WorkingDaysServiceSpec.scala
+++ test/services/WorkingDaysServiceSpec.scala
@@ -17,10 +17,13 @@
 package services
 
 import java.time.LocalDate
+import java.time.LocalDate.{of => d}
 import java.util.concurrent.TimeoutException
 
 import connectors.BankHolidaysConnector
+import org.joda.time.{LocalDate => JodaDate}
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.Inspectors
 import play.api.cache.CacheApi
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.UnitSpec
@@ -32,43 +35,49 @@ import scala.language.postfixOps
 import scala.reflect.ClassTag
 
 
-class WorkingDaysServiceSpec extends UnitSpec with MockFactory {
+class WorkingDaysServiceSpec extends UnitSpec with MockFactory with Inspectors {
 
-  val fixedHolidaySet: BankHolidaySet =
-    BankHolidaySet("england-and-wales",
-      List(
-        BankHoliday("some holiday", new org.joda.time.LocalDate(2017, 3, 24)),
-        //March 25,26 is weekend
-        BankHoliday("some holiday", new org.joda.time.LocalDate(2017, 3, 27))
-      ))
+  private case class Test(date: LocalDate, daysToAdd: Int, expected: LocalDate)
 
-  class Setup {
-
+  private class Setup {
     val mockConnector = mock[BankHolidaysConnector]
     val mockCache = mock[CacheApi]
-
-
   }
+
+  val fixedHolidaySet: BankHolidaySet =
+    BankHolidaySet("england-and-wales", List(
+      BankHoliday("some holiday", new JodaDate(2017, 3, 24)),
+      //March 25,26 is weekend
+      BankHoliday("some holiday", new JodaDate(2017, 3, 27))
+    ))
 
   "addWorkingDays" must {
 
-    "should skip over weekends as well as bank holidays" in new Setup {
+    "skip over weekends as well as bank holidays" in {
 
-      (mockCache.getOrElse[BankHolidaySet](_: String, _: Duration)(_: BankHolidaySet)(_: ClassTag[BankHolidaySet]))
-        .expects(WorkingDaysService.BANK_HOLIDAYS_CACHE_KEY, 1 day, *, *).returns(fixedHolidaySet)
+      val tests = Seq[Test](
+        Test(date = d(2017, 3, 22), daysToAdd = 1, expected = d(2017, 3, 23)),
+        Test(date = d(2017, 3, 22), daysToAdd = 2, expected = d(2017, 3, 28)),
+        Test(date = d(2017, 3, 23), daysToAdd = 1, expected = d(2017, 3, 28)),
+        Test(date = d(2017, 3, 23), daysToAdd = 2, expected = d(2017, 3, 29))
+      )
 
-      (mockConnector.bankHolidays(_: String)(_: HeaderCarrier))
-        .expects("england-and-wales", *)
-        .returns(Future.successful(fixedHolidaySet)).once()
+      forAll(tests) { test =>
+        new Setup {
+          (mockCache.getOrElse[BankHolidaySet](_: String, _: Duration)(_: BankHolidaySet)(_: ClassTag[BankHolidaySet]))
+            .expects(WorkingDaysService.BANK_HOLIDAYS_CACHE_KEY, 1 day, *, *).returns(fixedHolidaySet)
+          (mockConnector.bankHolidays(_: String)(_: HeaderCarrier))
+            .expects("england-and-wales", *)
+            .returns(Future.successful(fixedHolidaySet)).once()
 
-      //must setup mocks prior to calling new constructor, as one mock is called during construction
-      val service = new WorkingDaysService(mockConnector, mockCache, mockConnector)
-
-      val date = LocalDate.of(2017, 3, 23)
-      service.addWorkingDays(date, 1) shouldBe LocalDate.of(2017, 3, 28)
+          //must setup mocks prior to calling new constructor, as one mock is called during construction
+          val service = new WorkingDaysService(mockConnector, mockCache, mockConnector)
+          service.addWorkingDays(test.date, test.daysToAdd) shouldBe test.expected
+        }
+      }
     }
 
-    "should call bank holiday connector when nothing found in cache" in new Setup {
+    "should call bank holiday connector when nothing found date cache" in new Setup {
 
       (mockCache.getOrElse[BankHolidaySet](_: String, _: Duration)(_: BankHolidaySet)(_: ClassTag[BankHolidaySet]))
         .expects(WorkingDaysService.BANK_HOLIDAYS_CACHE_KEY, 1 day, *, *).onCall(product => {
@@ -90,14 +99,14 @@ class WorkingDaysServiceSpec extends UnitSpec with MockFactory {
     }
 
 
-    "should call bank holiday connector when nothing found in cache and failed to download file from Web" in new Setup {
+    "should call bank holiday connector when nothing found date cache and failed to download file from Web" in new Setup {
 
       /*
       here the sequence of calls is important:
       1.) on creating the service instance a call to FallbackBankHolidaysConnector
           (subtype of BankHolidaysConnector) is made to load the holiday schedule from file on classpath
       2.) then when the cache is consulted, no value is found
-      3.) then the _second_ call to BankHolidaysConnector fails with a timed out future
+      3.) then the _second_ call to BankHolidaysConnector fails with a timed expected future
           which will cause the default holiday schedule to be used temporarily
        */
       inSequence {
