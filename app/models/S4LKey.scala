7e9cc08ea4a028cc0762573b6300045d9ae4a00b
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2017-04-03 18:01:30 +0100

    SCRS-5928 Temporary contracts page and tests

--- app/models/S4LKey.scala
+++ app/models/S4LKey.scala
@@ -18,7 +18,7 @@ package models
 
 import models.view.sicAndCompliance.BusinessActivityDescription
 import models.view.sicAndCompliance.cultural.NotForProfit
-import models.view.sicAndCompliance.labour.Workers
+import models.view.sicAndCompliance.labour.{TemporaryContracts, Workers}
 import models.view.test.SicStub
 import models.view.vatFinancials._
 import models.view.vatTradingDetails.{StartDateView, TaxableTurnover, TradingNameView, VoluntaryRegistration}
@@ -53,5 +53,6 @@ object S4LKey {
   implicit val sicStub: S4LKey[SicStub] = S4LKey("SicStub")
   implicit val notForProfit: S4LKey[NotForProfit] = S4LKey("NotForProfit")
   implicit val workers: S4LKey[Workers] = S4LKey("Workers")
+  implicit val temporaryContracts: S4LKey[TemporaryContracts] = S4LKey("TemporaryContracts")
 
 }
