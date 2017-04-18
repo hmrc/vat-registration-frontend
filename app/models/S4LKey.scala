62fd09266649d3dcb6e7325d60467c4a23fd96b1
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2017-04-18 11:47:26 +0100

    SCRS-6176 Correcting S4LKey conflicts

--- app/models/S4LKey.scala
+++ app/models/S4LKey.scala
@@ -18,8 +18,7 @@ package models
 
 import models.view.sicAndCompliance.BusinessActivityDescription
 import models.view.sicAndCompliance.cultural.NotForProfit
-import models.view.sicAndCompliance.financial.{AdviceOrConsultancy, ChargeFees}
-import models.view.sicAndCompliance.financial.{ActAsIntermediary, AdviceOrConsultancy}
+import models.view.sicAndCompliance.financial.{ActAsIntermediary, AdviceOrConsultancy, ChargeFees}
 import models.view.sicAndCompliance.labour.{SkilledWorkers, _}
 import models.view.test.SicStub
 import models.view.vatFinancials._
@@ -60,14 +59,13 @@ object S4LKey {
   implicit val companyBankAccountDetailsS4LKey: S4LKey[CompanyBankAccountDetails] = S4LKey("CompanyBankAccountDetails")
   implicit val businessActivityDescriptionS4LKey: S4LKey[BusinessActivityDescription] = S4LKey("BusinessActivityDescription")
   implicit val sicStub: S4LKey[SicStub] = S4LKey("SicStub")
-  implicit val companyProvideWorkers: S4LKey[CompanyProvideWorkers] = S4LKey("CompanyProvideWorkers")
   implicit val notForProfit: S4LKey[NotForProfit] = S4LKey("NotForProfit")
+  implicit val companyProvideWorkers: S4LKey[CompanyProvideWorkers] = S4LKey("CompanyProvideWorkers")
   implicit val workers: S4LKey[Workers] = S4LKey("Workers")
-  implicit val adviceOrConsultancy: S4LKey[AdviceOrConsultancy] = S4LKey("AdviceOrConsultancy")
-  implicit val chargeFees: S4LKey[ChargeFees] = S4LKey("ChargeFees")
   implicit val temporaryContracts: S4LKey[TemporaryContracts] = S4LKey("TemporaryContracts")
   implicit val skilledWorkers: S4LKey[SkilledWorkers] = S4LKey("SkilledWorkers")
   implicit val adviceOrConsultancy: S4LKey[AdviceOrConsultancy] = S4LKey("AdviceOrConsultancy")
   implicit val actAsIntermediary: S4LKey[ActAsIntermediary] = S4LKey("ActAsIntermediary")
+  implicit val chargeFees: S4LKey[ChargeFees] = S4LKey("ChargeFees")
 
 }
