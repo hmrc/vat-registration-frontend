947c4909d722c50bceefeeb40e7c6cb13cb8c7ea
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-04-21 17:43:37 +0100

    created a test endpoint to test consuming COMP_REG
    created CT Connector

--- app/services/IncorporationInformationService.scala
+++ app/services/IncorporationInformationService.scala
@@ -17,10 +17,12 @@
 package services
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 import cats.data.OptionT
 import com.google.inject.ImplementedBy
+import connectors.CTConnector
 import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -33,13 +35,19 @@ trait IIService {
 
 }
 
-class IncorporationInformationService @Inject()() extends IIService {
+class IncorporationInformationService @Inject()(ctConnector: CTConnector) extends IIService with CommonService {
 
   import cats.instances.future._
 
+  val formatter = DateTimeFormatter.ofPattern("YYYY-mm-dd")
+
   def getCTActiveDate()(implicit headerCarrier: HeaderCarrier): OptionT[Future, LocalDate] =
-//    OptionT.pure(LocalDate.of(2017, 8, 4)) // is filtered out, no option is shown
-      OptionT.pure(LocalDate.now()) // will be shown as radio button
+    OptionT.liftF(fetchRegistrationId.flatMap(ctConnector.getRegistration)).flatMap {
+      ctr => OptionT.fromOption(ctr.accountingDetails.flatMap(_.activeDate.map(LocalDate.parse(_, formatter))))
+    }
+
+  //    OptionT.pure(LocalDate.of(2017, 8, 4)) // is filtered out, no option is shown
+  //      OptionT.pure(LocalDate.now()) // will be shown as radio button
   //    OptionT.none // we couldn't get the CT active date, no radio button will be shown
 
 }
