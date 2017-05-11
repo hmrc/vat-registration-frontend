0275b5561573bb49a154b9893bcf6431d6bec7a5
Darrell Plant <darrell.plant@gmail.com>
2017-05-11 14:57:50 +0100

    Implement Officer DOB page and FE model

---
+++ app/forms/vatLodgingOfficer/OfficerDateOfBirthForm.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.vatLodgingOfficer
+
+import java.time.LocalDate
+
+import forms.FormValidation.Dates.{nonEmptyDateModel, validDateModel}
+import forms.FormValidation._
+import models.DateModel
+import models.view.vatLodgingOfficer.OfficerDateOfBirthView
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
+
+object OfficerDateOfBirthForm {
+
+  implicit val errorCode: ErrorCode = "officerDOB"
+
+  implicit object LocalDateOrdering extends Ordering[LocalDate] {
+    override def compare(x: LocalDate, y: LocalDate): Int = x.compareTo(y)
+  }
+
+  val minDate: LocalDate = LocalDate.now() minusYears 200
+  val maxDate: LocalDate = LocalDate.now() minusYears 18
+
+  val form = Form(
+    mapping (
+      "dob" -> mapping(
+        "day" -> text,
+        "month" -> text,
+        "year" -> text
+      )(DateModel.apply)(DateModel.unapply).verifying(nonEmptyDateModel(validDateModel(inRange(minDate, maxDate))))
+    ) (OfficerDateOfBirthView.bind)(OfficerDateOfBirthView.unbind)
+  )
+}
