251f125a9a156d20193d73120d41742dab011c1f
Reddy Yattpu <reddy.yattpu@digital.hmrc.gov.uk>
2017-05-22 10:42:43 +0100

    SCRS-6715-FE-Create-screen-Who-are-you

--- app/controllers/vatLodgingOfficer/CompletionCapacityController.scala
+++ app/controllers/vatLodgingOfficer/CompletionCapacityController.scala
@@ -66,9 +66,9 @@ class CompletionCapacityController @Inject()(ds: CommonPlayDependencies)
               officerSeq <- fetchOfficerList().getOrElse(Seq())
               officer = officerSeq.find(_.name.id == form.id)
               _ <- s4l.saveForm(CompleteCapacityView(form.id, officer))
-            } yield Redirect(controllers.sicAndCompliance.routes.BusinessActivityDescriptionController.show())
+            } yield Redirect(controllers.vatLodgingOfficer.routes.OfficerDateOfBirthController.show())
           )
-      )
+      )git
   }
 
 }
