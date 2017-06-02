316d61228b3f8f6d37b4c61b453944adee2ed3bc
Fi Martin <feeon@live.com>
2017-06-02 14:40:23 +0100

    SCRS-7155 added unit tests

---
+++ test/controllers/vatLodgingOfficer/PreviousAddressQuestionControllerSpec.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.vatLodgingOfficer
+
+import controllers.vatLodgingOfficer
+import fixtures.VatRegistrationFixture
+import helpers.{S4LMockSugar, VatRegSpec}
+import models.view.vatLodgingOfficer.PreviousAddressQuestionView
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import play.api.test.FakeRequest
+
+class PreviousAddressQuestionControllerSpec extends VatRegSpec with VatRegistrationFixture with S4LMockSugar {
+
+  object TestPreviousAddressQuestionController extends PreviousAddressQuestionController(ds)(mockS4LService, mockVatRegistrationService) {
+    override val authConnector = mockAuthConnector
+  }
+
+  val fakeRequest = FakeRequest(vatLodgingOfficer.routes.PreviousAddressQuestionController.show())
+
+  s"GET ${vatLodgingOfficer.routes.PreviousAddressQuestionController.show()}" should {
+
+    reset(mockVatRegistrationService)
+
+    "return HTML when there's a previous address question in S4L" in {
+      save4laterReturns2(PreviousAddressQuestionView(yesNo = true))()
+      when(mockVatRegistrationService.getVatScheme()(any())).thenReturn(validVatScheme.pure)
+
+      callAuthorised(TestPreviousAddressQuestionController.show) {
+        _ includesText "Have you lived at your current address for 3 years or more?"
+      }
+    }
+
+    "return HTML when there's nothing in S4L and vatScheme contains data" in {
+      save4laterReturnsNothing2[PreviousAddressQuestionView]()
+      when(mockVatRegistrationService.getVatScheme()(any())).thenReturn(validVatScheme.pure)
+
+      callAuthorised(TestPreviousAddressQuestionController.show) {
+        _ includesText "Have you lived at your current address for 3 years or more?"
+      }
+    }
+
+    "return HTML when there's nothing in S4L and vatScheme contains no data" in {
+      save4laterReturnsNothing2[PreviousAddressQuestionView]()
+      when(mockVatRegistrationService.getVatScheme()(any())).thenReturn(emptyVatScheme.pure)
+
+      callAuthorised(TestPreviousAddressQuestionController.show) {
+        _ includesText "Have you lived at your current address for 3 years or more?"
+      }
+    }
+  }
+
+  s"POST ${vatLodgingOfficer.routes.PreviousAddressQuestionController.submit()} with Empty data" should {
+
+    "return 400" in {
+      submitAuthorised(TestPreviousAddressQuestionController.submit(), fakeRequest.withFormUrlEncodedBody(
+      )) {
+        result => result isA 400
+      }
+
+    }
+  }
+
+  s"POST ${vatLodgingOfficer.routes.PreviousAddressQuestionController.submit()} with previous address question no selected" should {
+
+    "return 303" in {
+      save4laterExpectsSave[PreviousAddressQuestionView]()
+
+      submitAuthorised(TestPreviousAddressQuestionController.submit(), fakeRequest.withFormUrlEncodedBody(
+        "previousAddressQuestionRadio" -> "false"
+      )) {
+        _ redirectsTo s"$contextRoot/business-contact"
+      }
+
+    }
+  }
+
+  s"POST ${vatLodgingOfficer.routes.PreviousAddressQuestionController.submit()} with previous address question yes selected" should {
+
+    "return 303" in {
+      save4laterExpectsSave[PreviousAddressQuestionView]()
+      submitAuthorised(TestPreviousAddressQuestionController.submit(), fakeRequest.withFormUrlEncodedBody(
+        "previousAddressQuestionRadio" -> "true"
+      )) {
+        _ redirectsTo s"$contextRoot/business-contact"
+      }
+
+    }
+  }
+
+}
