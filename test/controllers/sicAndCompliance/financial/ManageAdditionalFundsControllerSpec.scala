94d0635af15ef2595c9068f39ee61a1089b035bd
Darrell Plant <darrell.plant@gmail.com>
2017-06-08 15:21:04 +0100

    update unit tests

--- test/controllers/sicAndCompliance/financial/ManageAdditionalFundsControllerSpec.scala
+++ test/controllers/sicAndCompliance/financial/ManageAdditionalFundsControllerSpec.scala
@@ -16,26 +16,19 @@
 
 package controllers.sicAndCompliance.financial
 
-import builders.AuthBuilder
 import fixtures.VatRegistrationFixture
-import helpers.VatRegSpec
-import models.S4LKey
+import helpers.{S4LMockSugar, VatRegSpec}
 import models.view.sicAndCompliance.financial.ManageAdditionalFunds
 import org.mockito.Matchers
 import org.mockito.Mockito._
 import play.api.http.Status
-import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import services.VatRegistrationService
-import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.Future
 
-class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistrationFixture {
-
-
+class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistrationFixture with S4LMockSugar {
 
   object ManageAdditionalFundsController extends ManageAdditionalFundsController(ds)(mockS4LService, mockVatRegistrationService) {
     override val authConnector = mockAuthConnector
@@ -46,10 +39,7 @@ class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistratio
   s"GET ${routes.ManageAdditionalFundsController.show()}" should {
 
     "return HTML when there's a Manage Additional Funds model in S4L" in {
-      val manageAdditionalFunds = ManageAdditionalFunds(true)
-
-      when(mockS4LService.fetchAndGet[ManageAdditionalFunds]()(Matchers.any(), Matchers.any(), Matchers.any()))
-        .thenReturn(Future.successful(Some(manageAdditionalFunds)))
+      save4laterReturns2(ManageAdditionalFunds(true))()
 
       submitAuthorised(ManageAdditionalFundsController.show(), fakeRequest.withFormUrlEncodedBody(
         "manageAdditionalFundsRadio" -> ""
@@ -59,12 +49,8 @@ class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistratio
     }
 
     "return HTML when there's nothing in S4L and vatScheme contains data" in {
-      when(mockS4LService.fetchAndGet[ManageAdditionalFunds]()
-        (Matchers.eq(S4LKey[ManageAdditionalFunds]), Matchers.any(), Matchers.any()))
-        .thenReturn(Future.successful(None))
-
-      when(mockVatRegistrationService.getVatScheme()(Matchers.any()))
-        .thenReturn(Future.successful(validVatScheme))
+      save4laterReturnsNothing2[ManageAdditionalFunds]()
+      when(mockVatRegistrationService.getVatScheme()(Matchers.any())).thenReturn(Future.successful(validVatScheme))
 
       callAuthorised(ManageAdditionalFundsController.show) {
        _ includesText "Does the company manage any funds that are not included in this list?"
@@ -73,12 +59,8 @@ class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistratio
   }
 
   "return HTML when there's nothing in S4L and vatScheme contains no data" in {
-    when(mockS4LService.fetchAndGet[ManageAdditionalFunds]()
-      (Matchers.eq(S4LKey[ManageAdditionalFunds]), Matchers.any(), Matchers.any()))
-      .thenReturn(Future.successful(None))
-
-    when(mockVatRegistrationService.getVatScheme()(Matchers.any[HeaderCarrier]()))
-      .thenReturn(Future.successful(emptyVatScheme))
+    save4laterReturnsNothing2[ManageAdditionalFunds]()
+    when(mockVatRegistrationService.getVatScheme()(Matchers.any[HeaderCarrier]())).thenReturn(Future.successful(emptyVatScheme))
 
     callAuthorised(ManageAdditionalFundsController.show) {
      _ includesText "Does the company manage any funds that are not included in this list?"
@@ -93,45 +75,32 @@ class ManageAdditionalFundsControllerSpec extends VatRegSpec with VatRegistratio
         result =>
           status(result) mustBe Status.BAD_REQUEST
       }
-
     }
   }
 
   s"POST ${routes.ManageAdditionalFundsController.submit()} with Manage Additional Funds Yes selected" should {
 
     "return 303" in {
-      val returnCacheMapManageAdditionalFunds = CacheMap("", Map("" -> Json.toJson(ManageAdditionalFunds(true))))
-
-      when(mockS4LService.save[ManageAdditionalFunds]
-        (Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any()))
-        .thenReturn(Future.successful(returnCacheMapManageAdditionalFunds))
-
+      save4laterExpectsSave[ManageAdditionalFunds]()
       submitAuthorised(ManageAdditionalFundsController.submit(), fakeRequest.withFormUrlEncodedBody(
         "manageAdditionalFundsRadio" -> "true"
       )) {
         response =>
           response redirectsTo s"$contextRoot/business-bank-account"
       }
-
     }
   }
 
   s"POST ${routes.ManageAdditionalFundsController.submit()} with Manage Additional Funds No selected" should {
 
     "return 303" in {
-      val returnCacheMapManageAdditionalFunds = CacheMap("", Map("" -> Json.toJson(ManageAdditionalFunds(false))))
-
-      when(mockS4LService.save[ManageAdditionalFunds]
-        (Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any()))
-        .thenReturn(Future.successful(returnCacheMapManageAdditionalFunds))
-
+      save4laterExpectsSave[ManageAdditionalFunds]()
       submitAuthorised(ManageAdditionalFundsController.submit(), fakeRequest.withFormUrlEncodedBody(
         "manageAdditionalFundsRadio" -> "false"
       )) {
         response =>
           response redirectsTo s"$contextRoot/business-bank-account"
       }
-
     }
   }
 }
