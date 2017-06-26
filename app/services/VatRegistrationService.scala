644516aa562d784c5171cfaa1a2b102803e57ff2
Darrell Plant <darrell.plant@gmail.com>
2017-06-26 15:50:11 +0100

    test coverage for persistence code

--- app/services/VatRegistrationService.scala
+++ app/services/VatRegistrationService.scala
@@ -191,7 +191,7 @@ class VatRegistrationService @Inject()(s4LService: S4LService,
         vs.vatServiceEligibility.getOrElse(throw fail("VatServiceEligibility"))
       ) { s4l =>
         update(s4l.vatEligibility)
-          .apply(vs.vatServiceEligibility.getOrElse(VatServiceEligibility())) //TODO remove the "seeding" with empty
+          .apply(vs.vatServiceEligibility.getOrElse(VatServiceEligibility()))
       }
 
     for {
