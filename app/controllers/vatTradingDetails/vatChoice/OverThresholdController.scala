bccb0292af23fee2a6bc8a8996497926fecc2de4
Peter Hazell <peter.hazell@digital.hmrc.gov.uk>
2017-07-24 17:18:37 +0100

    SCRS-8040 Initial threshold q1 page work

---
+++ app/controllers/vatTradingDetails/vatChoice/OverThresholdController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.vatTradingDetails.vatChoice
+
+import javax.inject.Inject
+
+import cats.syntax.FlatMapSyntax
+import controllers.{CommonPlayDependencies, VatRegistrationController}
+import forms.vatTradingDetails.vatChoice.OverThresholdFormFactory
+import models.view.vatTradingDetails.vatChoice.OverThresholdView
+import play.api.data.Form
+import play.api.mvc._
+import services.{S4LService, VatRegistrationService}
+
+class OverThresholdController @Inject()(overThresholdFormFactory: OverThresholdFormFactory, ds: CommonPlayDependencies)
+                                       (implicit s4LService: S4LService, vrs: VatRegistrationService)
+  extends VatRegistrationController(ds) with FlatMapSyntax {
+
+  val form: Form[OverThresholdView] = overThresholdFormFactory.form
+
+  def show: Action[AnyContent] = authorised.async(implicit user => implicit request =>
+    viewModel[OverThresholdView]().fold(form)(form.fill)
+      .map(f => Ok(views.html.pages.vatTradingDetails.vatChoice.over_threshold((f)))))
+
+  def submit: Action[AnyContent] = authorised.async(implicit user => implicit request =>
+    form.bindFromRequest().fold(
+      badForm => BadRequest(views.html.pages.vatTradingDetails.vatChoice.over_threshold(badForm)).pure,
+      data => save(data).map(_ => Redirect(controllers.vatTradingDetails.vatChoice.routes.VoluntaryRegistrationController.show()))))
+
+}
