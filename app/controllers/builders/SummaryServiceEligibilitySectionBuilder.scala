922f5707a2bbc7d7c444a7c953ca565cdb5b5236
christopher.walker <christopher.walker@digital.hmrc.gov.uk>
2017-10-05 14:05:15 +0100

    getUrl fix

--- app/controllers/builders/SummaryServiceEligibilitySectionBuilder.scala
+++ app/controllers/builders/SummaryServiceEligibilitySectionBuilder.scala
@@ -91,7 +91,7 @@ case class SummaryServiceEligibilitySectionBuilder(vatServiceEligibility: Option
   )
 
   def getUrl(serviceName: String, uri: String): Call = {
-    val basePath = baseUrl(serviceName)
+    val basePath = getConfString(s"$serviceName.www.host", throw new RuntimeException(s"Can't find host for $serviceName"))
     val mainUri = getConfString(s"$serviceName.uri","/register-for-vat/")
     val serviceUri = getConfString(s"$serviceName.uris.$uri",uri)
     Call("Get", s"$basePath$mainUri$serviceUri")
