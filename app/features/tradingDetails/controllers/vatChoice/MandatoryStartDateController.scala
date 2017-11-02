866e83eebbf1a37811965e6359f85c44f3adde8b
James Mountain <james.mountain@digital.hmrc.gov.uk>
2017-11-02 14:16:48 +0000

    SCRS-5321 Added post incorp vat start date pages for mandatory/voluntary

--- app/features/tradingDetails/controllers/vatChoice/MandatoryStartDateController.scala
+++ app/features/tradingDetails/controllers/vatChoice/MandatoryStartDateController.scala
@@ -16,27 +16,71 @@
 
 package controllers.vatTradingDetails.vatChoice
 
+import java.time.LocalDate
 import javax.inject.Inject
 
 import connectors.KeystoreConnector
 import controllers.{CommonPlayDependencies, VatRegistrationController}
 import play.api.mvc._
 import services.{S4LService, SessionProfile, VatRegistrationService}
+import features.tradingDetails.views.html.vatChoice.{mandatory_start_date_confirmation, mandatory_start_date_incorp}
+import forms.vatTradingDetails.vatChoice.MandatoryStartDateForm
+import models.{CurrentProfile, MonthYearModel}
+import models.api.{VatExpectedThresholdPostIncorp, VatThresholdPostIncorp}
+import models.view.vatTradingDetails.vatChoice.StartDateView
+import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.Future
 
-class MandatoryStartDateController @Inject()(s4LService: S4LService,
-                                             ds: CommonPlayDependencies)
-                                            (implicit vrs: VatRegistrationService)
+class MandatoryStartDateController @Inject()(ds: CommonPlayDependencies)
+                                            (implicit vrs: VatRegistrationService, s4LService: S4LService)
   extends VatRegistrationController(ds) with SessionProfile {
 
   val keystoreConnector: KeystoreConnector = KeystoreConnector
 
+  def calculateMandatoryStartDate(threshold : Option[VatThresholdPostIncorp], expectedThreshold : Option[VatExpectedThresholdPostIncorp]): Option[LocalDate] = {
+    def calculatedCrossedThresholdDate(thresholdDate : LocalDate) = thresholdDate.withDayOfMonth(1).plusMonths(2)
+
+    (threshold.flatMap(_.overThresholdDate), expectedThreshold.flatMap(_.expectedOverThresholdDate)) match {
+      case (Some(td), Some(ed)) =>
+        val calculatedThresholdDate = calculatedCrossedThresholdDate(td)
+        Some(if (calculatedThresholdDate.isBefore(ed)) calculatedThresholdDate else ed)
+      case (Some(td), None) => Some(calculatedCrossedThresholdDate(td))
+      case (None, Some(ed)) => Some(ed)
+      case _ => None
+    }
+  }
+
+  def mandatoryStartDate(implicit profile : CurrentProfile, hc : HeaderCarrier) : Future[Option[LocalDate]] = {
+    vrs.getVatScheme().map(
+      _.vatServiceEligibility.flatMap(
+        _.vatEligibilityChoice match {
+          case Some(vec) => calculateMandatoryStartDate(vec.vatThresholdPostIncorp, vec.vatExpectedThresholdPostIncorp)
+          case None => None
+        }
+      )
+    )
+  }
+
   def show: Action[AnyContent] = authorised.async {
     implicit user =>
       implicit request =>
-        withCurrentProfile { _ =>
-          Future.successful(Ok(features.tradingDetails.views.html.vatChoice.mandatory_start_date_confirmation()))
+        withCurrentProfile { implicit profile =>
+          profile.incorporationDate.fold(Future.successful(Ok(mandatory_start_date_confirmation()))) {date =>
+            mandatoryStartDate.flatMap(calculatedDate =>
+              viewModel[StartDateView]().getOrElse(StartDateView()).map {vm =>
+                val viewModel = (calculatedDate, vm.date) match {
+                  case (Some(calcDate), Some(dt)) if calcDate != dt => vm.copy(dateType = StartDateView.SPECIFIC_DATE)
+                  case _ => vm
+                }
+                val calculatedDateValue = calculatedDate.getOrElse(throw new RuntimeException(""))
+                Ok(mandatory_start_date_incorp(
+                  MandatoryStartDateForm.form(date, calculatedDateValue).fill(viewModel),
+                  calculatedDateValue.format(MonthYearModel.FORMAT_D_MMMM_Y)
+                ))
+              }
+            )
+          }
         }
   }
 
@@ -44,8 +88,25 @@ class MandatoryStartDateController @Inject()(s4LService: S4LService,
     implicit user =>
       implicit request =>
         withCurrentProfile { implicit profile =>
-          vrs.submitTradingDetails().map { _ =>
-            Redirect(controllers.vatFinancials.vatBankAccount.routes.CompanyBankAccountController.show())
+          profile.incorporationDate match {
+            case Some(incorpDate) =>
+              mandatoryStartDate.flatMap {calculatedDate =>
+                val calculatedDateValue = calculatedDate.getOrElse(throw new RuntimeException(""))
+                MandatoryStartDateForm.form(incorpDate, calculatedDateValue).bindFromRequest().fold(
+                  badForm => BadRequest(mandatory_start_date_incorp(
+                    badForm,
+                    calculatedDateValue.format(MonthYearModel.FORMAT_D_MMMM_Y)
+                  )).pure,
+                  goodForm => save(goodForm).flatMap { _ =>
+                    vrs.submitTradingDetails().map(_ =>
+                      Redirect(controllers.vatFinancials.vatBankAccount.routes.CompanyBankAccountController.show())
+                    )
+                  }
+                )
+              }
+            case None => vrs.submitTradingDetails().map { _ =>
+              Redirect(controllers.vatFinancials.vatBankAccount.routes.CompanyBankAccountController.show())
+            }
           }
         }
   }
