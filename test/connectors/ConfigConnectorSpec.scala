077228197242a924171c27334918c18d2cac0bf7
Chris Poole <christopher.poole@digital.hmrc.gov.uk>
2018-01-03 07:37:45 +0000

    Updated copyright to 2018

--- test/connectors/ConfigConnectorSpec.scala
+++ test/connectors/ConfigConnectorSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,4 +49,3 @@ class ConfigConnectorSpec extends VatRegSpec with VatRegistrationFixture {
   }
 
 }
-
