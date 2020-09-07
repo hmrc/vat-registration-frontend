a37a5a76e0fc155fad65ffd0b3dffd6de0901700
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2020-09-07 10:45:58 +0100

    SAR-6120 Created Honesty Declaration Page

---
+++ app/controllers/HonestyDeclarationController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.{AuthClientConnector, FrontendAppConfig}
+import connectors.KeystoreConnector
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SessionProfile
+import views.html.honesty_declaration
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class HonestyDeclarationController @Inject()(mcc: MessagesControllerComponents,
+                                             honestyDeclarationView: honesty_declaration,
+                                             val authConnector: AuthClientConnector,
+                                             val keystoreConnector: KeystoreConnector
+                                            )(implicit val appConfig: FrontendAppConfig,
+                                              executionContext: ExecutionContext)
+  extends BaseController(mcc) with SessionProfile {
+
+  val show: Action[AnyContent] = isAuthenticatedWithProfile {
+    implicit request =>
+      implicit profile =>
+        Future.successful(Ok(honestyDeclarationView(routes.HonestyDeclarationController.submit())))
+  }
+
+  val submit: Action[AnyContent] = isAuthenticatedWithProfile {
+    implicit request =>
+      implicit profile =>
+        Future.successful(Redirect(routes.OfficerController.showFormerName()))
+  }
+}
---
+++ it/controllers/HonestyDeclarationControllerISpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import itutil.IntegrationSpecBase
+import org.scalatest.concurrent.ScalaFutures
+import play.api.http.HeaderNames
+import play.api.libs.json.Json
+import play.api.libs.ws.WSResponse
+import support.AppAndStubs
+
+import scala.concurrent.Future
+
+class HonestyDeclarationControllerISpec extends IntegrationSpecBase with AppAndStubs with ScalaFutures {
+
+  val url: String = controllers.routes.HonestyDeclarationController.show().url
+
+  val userId = "user-id-12345"
+
+  s"GET $url" must {
+    "return an OK" in new StandardTestHelpers {
+      given()
+        .user.isAuthorised
+        .audit.writesAudit()
+
+      insertCurrentProfileIntoDb(currentProfile, sessionId)
+
+      val response: Future[WSResponse] = buildClient(url).get()
+      whenReady(response) { res =>
+        res.status mustBe 200
+      }
+    }
+  }
+
+  s"POST $url" must {
+    "return a redirect to Officer Name" in new StandardTestHelpers {
+      given()
+        .user.isAuthorised
+        .audit.writesAudit()
+
+      insertCurrentProfileIntoDb(currentProfile, sessionId)
+
+      val response: Future[WSResponse] = buildClient(url).post(Json.obj())
+      whenReady(response) { res =>
+        res.status mustBe 303
+        res.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.OfficerController.showFormerName().url)
+      }
+    }
+  }
+}
