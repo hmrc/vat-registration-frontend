65b2d12538cf2161da85729c9cf2fa19d3cfa5a4
Rob Hawkes <17453872+kabuumu@users.noreply.github.com>
2020-09-09 09:05:01 +0100

    SAR-6145 - Add new route to call into Incorp ID flow

---
+++ it/controllers/IncorpIdControllerISpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import featureswitch.core.config.{FeatureSwitching, StubIncorpIdJourney}
+import itutil.IntegrationSpecBase
+import org.scalatest.concurrent.IntegrationPatience
+import play.api.libs.json.Json
+import play.api.libs.ws.WSResponse
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{CREATED, await, _}
+import support.AppAndStubs
+
+class IncorpIdControllerISpec extends IntegrationSpecBase with AppAndStubs with FeatureSwitching with IntegrationPatience {
+  "/start-incorp-id-journey" should {
+    "redirect to the returned journey url" in {
+      implicit val request = FakeRequest()
+
+      disable(StubIncorpIdJourney)
+
+      given()
+        .user.isAuthorised
+
+      val testJourneyStartUrl = "/test"
+
+      stubPost("/incorporated-entity-identification/api/journey", CREATED, Json.obj("journeyStartUrl" -> testJourneyStartUrl).toString)
+
+      val res: WSResponse = await(buildClient(controllers.routes.IncorpIdController.startIncorpIdJourney().url).get)
+
+      res.status mustBe SEE_OTHER
+      res.header(LOCATION) mustBe Some(testJourneyStartUrl)
+
+    }
+  }
+}
---
+++ test/controllers/IncorpIdControllerSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.time.{LocalDate, LocalDateTime}
+
+import _root_.models._
+import fixtures.VatRegistrationFixture
+import mocks.TimeServiceMock
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito._
+import play.api.mvc.{Action, AnyContent, AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Result}
+import play.api.test.FakeRequest
+import services.VoluntaryPageViewModel
+import testHelpers.{ControllerSpec, FutureAssertions}
+
+import scala.concurrent.Future
+
+class IncorpIdControllerSpec extends ControllerSpec with VatRegistrationFixture with TimeServiceMock with FutureAssertions {
+
+  class Setup(cp: Option[CurrentProfile] = Some(currentProfile)) {
+    val testController: IncorpIdController = new IncorpIdController(
+      messagesControllerComponents,
+      mockAuthClientConnector,
+      mockKeystoreConnector,
+      mockIncorpIdService
+    )
+
+    mockAuthenticated()
+    mockWithCurrentProfile(cp)
+  }
+
+  "startIncorpIdJourney" should {
+    "redirect to the journeyStartUrl" in new Setup {
+      implicit val fakeRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
+      lazy val testContinueUrl: String = controllers.routes.OfficerController.showFormerName().absoluteURL()
+      lazy val testJourneyStartUrl = "/test"
+      mockCreateJourney(testContinueUrl)(Future.successful(testJourneyStartUrl))
+
+      lazy val res: Future[Result] = testController.startIncorpIdJourney()(fakeRequest)
+
+      status(res) mustBe SEE_OTHER
+      redirectLocation(res) mustBe Some(testJourneyStartUrl)
+    }
+  }
+}
