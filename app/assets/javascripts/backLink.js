2edaa7824e027cfed2bb43abf1783aa2770aa453
Simon Adams <6369605+palequezal@users.noreply.github.com>
2020-11-10 16:20:01 +0000

    Fix issues with CSP

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -1,4 +1,8 @@
-document.querySelector('#back-link[href="#"]').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.history.back();
-})
+var backLink = document.querySelector('#back-link[href="#"]')
+
+if (backLink != null) {
+    backLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.history.back();
+    })
+}
