2a7a1c8707e5f0d39ed063cc6703763ce364c1e5
EviNikolaidou <45890813+EviNikolaidou@users.noreply.github.com>
2020-12-09 08:56:46 +0000

    SAR-6766 Get applicant details and pass date of birth into form to validate

--- test/views/FormerNameDatePageSpec.scala
+++ test/views/FormerNameDatePageSpec.scala
@@ -34,7 +34,7 @@ class FormerNameDatePageSpec extends VatRegSpec with I18nSupport {
   implicit val messagesApi: MessagesApi = injector.instanceOf[MessagesApi]
   implicit val appConfig: FrontendAppConfig = injector.instanceOf[FrontendAppConfig]
 
-  lazy val form = FormerNameDateForm.form
+  lazy val form = FormerNameDateForm.form(testApplicantDob)
 
   "Former Name Date Page" should {
     "display the page without pre populated data" in {
