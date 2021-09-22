736381da8cf78b07114032842b8a8d8203656106
Nathan Farnell <63645158+NathanFarnell@users.noreply.github.com>
2021-09-22 15:25:58 +0100

    SAR-8255 Added Postal Cover Sheet Page

---
+++ app/controllers/registration/attachments/PostalCoverSheetController.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.registration.attachments
+
+import config.{AuthClientConnector, BaseControllerComponents, FrontendAppConfig}
+import connectors.KeystoreConnector
+import controllers.BaseController
+import play.api.mvc.{Action, AnyContent}
+import services.{SessionProfile, VatRegistrationService}
+import views.html.PostalCoverSheet
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class PostalCoverSheetController @Inject()(view: PostalCoverSheet,
+                                           val authConnector: AuthClientConnector,
+                                           val keystoreConnector: KeystoreConnector,
+                                           val vatRegistrationService: VatRegistrationService
+                                          )(implicit appConfig: FrontendAppConfig,
+                                            val executionContext: ExecutionContext,
+                                            baseControllerComponents: BaseControllerComponents)
+  extends BaseController with SessionProfile {
+
+  val show: Action[AnyContent] = isAuthenticatedWithProfileNoStatusCheck {
+    implicit request =>
+      implicit profile =>
+        vatRegistrationService.getAckRef(profile.registrationId).map(ackRef =>
+          Ok(view(ackRef))
+        )
+  }
+}
