4706bdb23aba1954e03db34b53ba6d890a78a0c8
jawilliams23 <63645131+jawilliams23@users.noreply.github.com>
2021-10-11 11:20:25 +0100

    SAR-8367 - Email Documents page added

---
+++ it/controllers/EmailDocumentsControllerISpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import featureswitch.core.config.FeatureSwitching
+import fixtures.ITRegistrationFixtures
+import itutil.ControllerISpec
+import play.api.libs.json.JsString
+import play.api.libs.ws.WSResponse
+
+import scala.concurrent.Future
+
+class EmailDocumentsControllerISpec extends ControllerISpec with ITRegistrationFixtures with FeatureSwitching {
+
+  val url: String = controllers.registration.attachments.routes.EmailDocumentsController.show().url
+
+  val testAckRef = "VRN1234567"
+
+  s"GET $url" must {
+    "return an OK" in new Setup {
+      given()
+        .user.isAuthorised
+        .audit.writesAudit()
+        .audit.writesAuditMerged()
+        .vatScheme.has("acknowledgement-reference", JsString(s"$testAckRef"))
+
+      insertCurrentProfileIntoDb(currentProfile, sessionId)
+
+      val response: Future[WSResponse] = buildClient(url).get()
+      whenReady(response) { res =>
+        res.status mustBe 200
+      }
+    }
+  }
+}
---
+++ test/views/attachments/EmailDocumentsViewSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.attachments
+
+import org.jsoup.Jsoup
+import play.twirl.api.Html
+import views.VatRegViewSpec
+import views.html.EmailDocuments
+
+class EmailDocumentsViewSpec extends VatRegViewSpec {
+
+  val emailDocumentsPage: EmailDocuments = app.injector.instanceOf[EmailDocuments]
+
+  val testRef = "VRN12345689"
+
+  lazy val view: Html = emailDocumentsPage(ackRef = testRef)
+  implicit val doc = Jsoup.parse(view.body)
+
+  object ExpectedContent {
+    val heading = "We need you to send the documents to the following HMRC email address"
+    val title = s"$heading - Register for VAT - GOV.UK"
+    val para1 = "For us to be able to progress your VAT registration quickly we need you to send these documents as soon as you have submitted the application form."
+    val panel1 = "vrs.newregistrations@hmrc.gov.uk"
+    val continue = "Save and continue"
+  }
+
+  "The Email Documents page" must {
+    "have a back link in new Setup" in new ViewSetup {
+      doc.hasBackLink mustBe true
+    }
+
+    "have the correct heading" in new ViewSetup {
+      doc.heading mustBe Some(ExpectedContent.heading)
+    }
+
+    "have the correct title" in new ViewSetup {
+      doc.title mustBe ExpectedContent.title
+    }
+
+    "have the correct text" in new ViewSetup {
+      doc.para(1) mustBe Some(ExpectedContent.para1)
+    }
+
+    "have the correct panel text" in new ViewSetup {
+      doc.select(Selectors.indent).get(0).text mustBe ExpectedContent.panel1
+    }
+
+    "have a primary action" in new ViewSetup {
+      doc.submitButton mustBe Some(ExpectedContent.continue)
+    }
+  }
+
+}
