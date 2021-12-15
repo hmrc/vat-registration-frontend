f8ad6f4f6fe816b85a2b2f14a8ede6b1f845ae9b
Kaizar Laxmidhar <kaizar.laxmidhar@digital.hmrc.gov.uk>
2021-12-15 13:15:49 +0000

    SAR-8735/SAR-8780 update partnership ID stub to handle all the partnership types

--- app/controllers/test/PartnershipIdentificationStubController.scala
+++ app/controllers/test/PartnershipIdentificationStubController.scala
@@ -16,9 +16,10 @@
 
 package controllers.test
 
+import models.api.{LtdLiabilityPartnership, LtdPartnership, PartyType, ScotLtdPartnership}
 import models.external.partnershipid.PartnershipIdJourneyConfig
 import models.external.{BvPass, PartnershipIdEntity}
-import play.api.libs.json.{JsString, Json}
+import play.api.libs.json.{JsObject, JsString, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
@@ -32,22 +33,50 @@ class PartnershipIdentificationStubController @Inject()(mcc: MessagesControllerC
 
   def createJourney(partyType: String): Action[PartnershipIdJourneyConfig] = Action(parse.json[PartnershipIdJourneyConfig]) {
     journeyConfig =>
-      val journeyId = if (journeyConfig.body.businessVerificationCheck) partyType + excludeBv else partyType
+      val journeyId = if (journeyConfig.body.businessVerificationCheck) s"$partyType:$excludeBv" else partyType
 
       Created(Json.obj("journeyStartUrl" -> JsString(journeyConfig.body.continueUrl + s"?journeyId=$journeyId")))
   }
 
   def retrieveValidationResult(journeyId: String): Action[AnyContent] = Action.async {
     Future.successful(
-      Ok(Json.toJson(PartnershipIdEntity(
-        sautr = Some("1234567890"),
-        postCode = if (journeyId.equals("1")) Some("AA11AA") else None,
-        registration = "REGISTERED",
-        businessVerification = if (journeyId.contains(excludeBv)) None else Some(BvPass),
-        bpSafeId = Some("testBpId"),
-        identifiersMatch = true
-      ))(PartnershipIdEntity.apiFormat))
+      Ok(partnershipResponse(journeyId))
     )
+  }
+
+  private def partnershipResponse(journeyId: String): JsValue = {
+    val partnershipEntity: JsValue = Json.toJson(PartnershipIdEntity(
+      sautr = Some("1234567890"),
+      postCode = Some("AA11AA"),
+      registration = "REGISTERED",
+      businessVerification = if (journeyId.contains(excludeBv)) None else Some(BvPass),
+      bpSafeId = Some("testBpId"),
+      identifiersMatch = true
+    ))(PartnershipIdEntity.apiFormat)
+
+    val partyType: PartyType = PartyType.fromString(journeyId.split(":").head)
+    partyType match {
+      case LtdPartnership | ScotLtdPartnership | LtdLiabilityPartnership =>
+        val companyProfile = Json.obj(
+        "companyProfile" -> Json.obj(
+          "companyName" -> "TestPartnership",
+          "companyNumber" -> "01234567",
+          "dateOfIncorporation" -> "2020-01-01",
+          "unsanitisedCHROAddress" -> Json.obj(
+            "address_line_1" -> "testLine1",
+            "address_line_2" -> "test town",
+            "care_of" -> "test name",
+            "country" -> "United Kingdom",
+            "locality" -> "test city",
+            "po_box" -> "123",
+            "postal_code" -> "AA11AA",
+            "premises" -> "1",
+            "region" -> "test region")
+          )
+        )
+        partnershipEntity.as[JsObject] ++ companyProfile
+      case _ => partnershipEntity
+    }
   }
 
 }
