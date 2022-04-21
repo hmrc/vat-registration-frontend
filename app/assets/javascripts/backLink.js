ab20634702a589dd286a0d75a322bc0fd5a78757
Simon Adams <6369605+palequezal@users.noreply.github.com>
2022-04-21 14:44:36 +0100

    SAR-9536: Fix 'return' link on save page

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -1,4 +1,4 @@
-var backLinks = document.querySelectorAll('a[href="#"][class="govuk-back-link"]')
+var backLinks = document.querySelectorAll('a[href="#"]')
 
 if (backLinks.length > 0) {
     backLinks.forEach(element => element.addEventListener('click', function(e) {
