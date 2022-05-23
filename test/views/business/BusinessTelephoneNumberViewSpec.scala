b251cb3771c5e2827f9763ff1cd07ac8fa2b7d23
Kaizar Laxmidhar <kaizar.laxmidhar@digital.hmrc.gov.uk>
2022-05-23 14:17:15 +0100

    SAR-9658 Add business telephone number page

---
+++ test/views/business/BusinessTelephoneNumberViewSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.business
+
+import forms.BusinessTelephoneNumberForm
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import views.VatRegViewSpec
+import views.html.business.BusinessTelephoneNumber
+
+class BusinessTelephoneNumberViewSpec extends VatRegViewSpec {
+  val view: BusinessTelephoneNumber = app.injector.instanceOf[BusinessTelephoneNumber]
+
+  object ExpectedContent {
+    val heading = "What is the business telephone number?"
+    val title = s"$heading - Register for VAT - GOV.UK"
+    val continue = "Save and continue"
+  }
+
+  implicit val doc: Document = Jsoup.parse(view(BusinessTelephoneNumberForm.form).body)
+
+  "Business Telephone number page" must {
+    "have a back link" in new ViewSetup {
+      doc.hasBackLink mustBe true
+    }
+
+    "have the correct heading" in new ViewSetup {
+      doc.heading mustBe Some(ExpectedContent.heading)
+    }
+
+    "have the correct page title" in new ViewSetup {
+      doc.title mustBe ExpectedContent.title
+    }
+
+    "have a primary action" in new ViewSetup {
+      doc.submitButton mustBe Some(ExpectedContent.continue)
+    }
+  }
+}
