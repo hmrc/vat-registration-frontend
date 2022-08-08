f9a20658dbaa0c7ec9b0267cf33a811f3e7b7631
Mahesh Mudi <102029646+mahesh-hmrc@users.noreply.github.com>
2022-08-08 14:54:44 +0100

    SAR-10195 | Toggle email/upload attachments based on UploadDocuments FS

--- test/views/attachments/ChooseAttachmentMethodViewSpec.scala
+++ test/views/attachments/ChooseAttachmentMethodViewSpec.scala
@@ -25,11 +25,6 @@ import views.html.attachments.ChooseAttachmentMethod
 
 class ChooseAttachmentMethodViewSpec extends VatRegViewSpec with FeatureSwitching {
 
-  val view = app.injector.instanceOf[ChooseAttachmentMethod]
-  val form = app.injector.instanceOf[AttachmentMethodForm]
-
-  implicit val doc = Jsoup.parse(view(form()).body)
-
   object ExpectedMessages {
     val heading = "How would you like to send the additional documents?"
     val p = "You can only select one method."
@@ -47,8 +42,6 @@ class ChooseAttachmentMethodViewSpec extends VatRegViewSpec with FeatureSwitchin
     val form = app.injector.instanceOf[AttachmentMethodForm]
     implicit val doc: Document = Jsoup.parse(view(form()).body)
 
-    disable(UploadDocuments)
-
     "have the correct page title" in new ViewSetup {
       doc.title must include(ExpectedMessages.heading)
     }
@@ -61,17 +54,22 @@ class ChooseAttachmentMethodViewSpec extends VatRegViewSpec with FeatureSwitchin
     }
 
     "have the correct radio options" in new ViewSetup {
-      doc.radio("3") mustBe Some(ExpectedMessages.post)
-      doc.radio("email") mustBe Some(ExpectedMessages.email)
-    }
-
-    "have the correct radio options when UploadDocuments FS is enabled" in new ViewSetup {
       doc.radio("2") mustBe Some(ExpectedMessages.upload)
       doc.radio("3") mustBe Some(ExpectedMessages.post)
-      doc.radio("email") mustBe Some(ExpectedMessages.email)
+      doc.radio("email") mustBe None
     }
+
     "have a save and continue button" in new ViewSetup {
       doc.submitButton mustBe Some(ExpectedMessages.button)
+    }
+
+    "have the correct radio options when UploadDocuments FS is disabled" in new ViewSetup {
+      disable(UploadDocuments)
+      implicit val docWithUploadDisabled: Document = Jsoup.parse(view(form()).body)
+
+      docWithUploadDisabled.radio("2") mustBe None
+      docWithUploadDisabled.radio("3") mustBe Some(ExpectedMessages.post)
+      docWithUploadDisabled.radio("email") mustBe Some(ExpectedMessages.email)
     }
   }
 
