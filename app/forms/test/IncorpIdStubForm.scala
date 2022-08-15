70fe51d0425b25e107479417d8fa74be0a37bbce
Simon Adams <6369605+palequezal@users.noreply.github.com>
2022-08-15 11:51:44 +0100

    Enhance Incorp ID stub page

---
+++ app/forms/test/IncorpIdStubForm.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.test
+
+import models.test.IncorpStubData
+import play.api.data.Form
+import play.api.data.Forms._
+
+object IncorpIdStubForm {
+
+  def form: Form[IncorpStubData] = Form[IncorpStubData](
+    mapping(
+      "crn" -> text,
+      "utr" -> optional(text),
+      "passBv" -> boolean
+    )(IncorpStubData.apply)(IncorpStubData.unapply)
+  )
+
+}
