d34e714dfd75ef1ef8e880e3bf83796ddc2602ad
Simon Adams <6369605+palequezal@users.noreply.github.com>
2022-11-09 11:40:32 +0000

    SAR-10868: Add Bank details a11y specs

--- a11y/pages/applicant/LeadPartnerEntityTypeA11ySpec.scala
+++ a11y/pages/applicant/LeadPartnerEntityTypeA11ySpec.scala
@@ -3,21 +3,38 @@ package pages.applicant
 
 import forms.PartnerForm
 import helpers.A11ySpec
+import views.html.applicant.PartnerEntityType
 
 class LeadPartnerEntityTypeA11ySpec extends A11ySpec {
-  val view = app.injector.instanceOf[lead_partner_entity_type]
-  val form = PartnerForm.form
+  val view = app.injector.instanceOf[PartnerEntityType]
+  val form = PartnerForm.form("value")
 
-  "the lead partner entity type page" when {
-    "the page is rendered without errors" must {
-      "pass all accessibility tests" in {
-        view(form).toString must passAccessibilityChecks
+  "the Partner entity type page" when {
+    "the user is a transactor" when {
+      "the page is rendered without errors" must {
+        "pass all accessibility tests" in {
+          view(form, isTransactor = true, 1).toString must passAccessibilityChecks
+        }
+      }
+
+      "the page is rendered with errors" must {
+        "pass all accessibility tests" in {
+          view(form.bind(Map.empty[String, String]), isTransactor = true, 1).toString must passAccessibilityChecks
+        }
       }
     }
 
-    "the page is rendered with errors" must {
-      "pass all accessibility tests" in {
-        view(form.bind(Map("value" -> ""))).toString must passAccessibilityChecks
+    "the user is not a transactor" when {
+      "the page is rendered without errors" must {
+        "pass all accessibility tests" in {
+          view(form, isTransactor = false, 1).toString must passAccessibilityChecks
+        }
+      }
+
+      "the page is rendered with errors" must {
+        "pass all accessibility tests" in {
+          view(form.bind(Map.empty[String, String]), isTransactor = false, 1).toString must passAccessibilityChecks
+        }
       }
     }
   }
