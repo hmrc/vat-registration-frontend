09044bda561a49ac77f1ce4e07e5f1830746fba8
hazal-gunduz <177622238+hazal-gunduz@users.noreply.github.com>
2025-05-09 09:20:46 +0100

    clean up

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -1,4 +1,4 @@
-var backLinks = document.querySelectorAll('a[href="#"]');
+var backLinks = document.querySelectorAll('a[href="#"]')
 
 if (backLinks.length > 0) {
     backLinks.forEach(element => element.addEventListener('click', function(e) {
