e7f82fcd4708987b2a7136ebec0c5ccba10c3860
Hazal Gunduz <177622238+hazal-gunduz@users.noreply.github.com>
2025-05-12 14:30:20 +0100

    fix for DL-16238 (#1201)
    * fix for DL-16238

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -3,6 +3,10 @@ var backLinks = document.querySelectorAll('a[href="#"]')
 if (backLinks.length > 0) {
     backLinks.forEach(element => element.addEventListener('click', function(e) {
         e.preventDefault();
-        window.history.back();
-    }))
+        if (document.referrer) {
+            window.location.href = document.referrer;
+        } else {
+            window.history.back();
+        }
+    }));
 }
