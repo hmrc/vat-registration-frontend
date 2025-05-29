25fcc289a26dfd7edc35c3ff8846e78d19fb6545
hazal-gunduz <177622238+hazal-gunduz@users.noreply.github.com>
2025-05-29 19:54:12 +0100

    fixing back button issue DL-16298

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -1,4 +1,4 @@
-var backLinks = document.querySelectorAll('a[href="#"]')
+var backLinks = document.querySelectorAll('a[href="#"]:not([data-module="hmrc-back-link"])')
 
 if (backLinks.length > 0) {
     backLinks.forEach(element => element.addEventListener('click', function(e) {
