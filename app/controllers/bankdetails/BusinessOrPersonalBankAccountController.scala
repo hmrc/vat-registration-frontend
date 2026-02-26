6d9a03165750ffe3228390fc736008fe637b9b9f
HG-Accenture <183600681+HG-Accenture@users.noreply.github.com>
2026-02-26 14:31:35 +0000

    Testing New page and Modification to existing HasCompanyBankAccountView

---
+++ app/controllers/bankdetails/BusinessOrPersonalBankAccountController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.bankdetails
+
+import config.{AuthClientConnector, BaseControllerComponents, FrontendAppConfig}
+import controllers.BaseController
+import forms.BusinessOrPersonalBankAccountForm
+import play.api.mvc.{Action, AnyContent}
+import services.{BankAccountDetailsService, SessionService}
+import views.html.bankdetails.BusinessOrPersonalBankAccountView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class BusinessOrPersonalBankAccountController @Inject()(
+                                                         val authConnector: AuthClientConnector,
+                                                         val bankAccountDetailsService: BankAccountDetailsService,
+                                                         val sessionService: SessionService,
+                                                         view: BusinessOrPersonalBankAccountView
+                                                       )(implicit appConfig: FrontendAppConfig,
+                                                         val executionContext: ExecutionContext,
+                                                         baseControllerComponents: BaseControllerComponents) extends BaseController {
+
+  def show: Action[AnyContent] = isAuthenticatedWithProfile {
+    implicit request => implicit profile =>
+      bankAccountDetailsService.fetchBankAccountDetails.map { bankDetails =>
+        val filledForm = bankDetails.map(_.isProvided).fold(BusinessOrPersonalBankAccountForm.form)(BusinessOrPersonalBankAccountForm.form.fill)
+        Ok(view(filledForm))
+      }
+  }
+
+  def submit: Action[AnyContent] = isAuthenticatedWithProfile {
+    implicit request => implicit profile =>
+      BusinessOrPersonalBankAccountForm.form.bindFromRequest().fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+        isProvided =>
+          bankAccountDetailsService.saveHasCompanyBankAccount(isProvided).map { _ =>
+            if (isProvided) {
+              //              This needs to be checked based on journey.
+              Redirect(routes.UkBankAccountDetailsController.show)
+            } else {
+              //              This needs to be checked based on journey.
+              Redirect(routes.NoUKBankAccountController.show)
+            }
+          }
+      )
+  }
+}
